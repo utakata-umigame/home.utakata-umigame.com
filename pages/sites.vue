0844c8c603cd66527ee3955cfd502d34edf63eb5
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-11-13 23:40:42 +0900

    First commit

---
+++ pages/sites.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="container">
+    <div class="links">
+      <a
+        href="https://utakata-umigame.com"
+        target="_blank"
+        class="button--green">Utakata</a>
+      <a
+        href="https://gallery.utakata-umigame.com"
+        target="_blank"
+        class="button--green">Gallery</a>
+      <a
+        href="https://wolf.utakata-umigame.com"
+        target="_blank"
+        class="button--green">Wolf</a>
+    </div>
+  </section>
+</template>
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
