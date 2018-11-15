8c4509d4b10c8f27e609249c91fb4d5380414036
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-11-15 19:13:00 +0900

    Update sites.vue

--- pages/sites.vue
+++ pages/sites.vue
@@ -1,67 +1,69 @@
 <template>
   <section class="container">
-    <el-row :gutter="12">
-      <el-col :span="8">
-        <el-card
-          :body-style="{'padding': '0'}">
-          <img
-            src="https://avatars2.githubusercontent.com/u/43202287?s=200&v=4"
-            class="image"
-            width="100%"
-            alt="aaa">
-          <div style="padding: 10px;">
-            <a
-              href="https://utakata-umigame.com"
-              target="_blank"
-              class="button--green">
-              安定版
-            </a>
-            <a
-              href="https://beta.utakata-umigame.com"
-              target="_blank"
-              class="button--green">
-              ベータ版
-            </a>
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card
-          :body-style="{'padding': '0'}">
-          <img
-            src="https://avatars2.githubusercontent.com/u/43202287?s=200&v=4"
-            class="image"
-            width="100%"
-            alt="aaa">
-          <div style="padding: 10px;">
-            <a
-              href="https://gallery.utakata-umigame.com"
-              target="_blank"
-              class="button--green">
-              リンク
-            </a>
-          </div>
-        </el-card>
-      </el-col>
-      <el-col :span="8">
-        <el-card
-          :body-style="{'padding': '0'}">
-          <img
-            src="https://avatars2.githubusercontent.com/u/43202287?s=200&v=4"
-            class="image"
-            width="100%"
-            alt="aaa">
-          <div style="padding: 10px;">
-            <a
-              href="https://wolf.utakata-umigame.com"
-              target="_blank"
-              class="button--green">
-              リンク
-            </a>
-          </div>
-        </el-card>
-      </el-col>
-    </el-row>
+    <transition name="el-fade-in">
+      <el-row :gutter="12">
+        <el-col :span="8">
+          <el-card
+            :body-style="{'padding': '0'}">
+            <img
+              src="https://avatars2.githubusercontent.com/u/43202287?s=200&v=4"
+              class="image"
+              width="100%"
+              alt="aaa">
+            <div style="padding: 10px;">
+              <a
+                href="https://utakata-umigame.com"
+                target="_blank"
+                class="button--green">
+                安定版
+              </a>
+              <a
+                href="https://beta.utakata-umigame.com"
+                target="_blank"
+                class="button--green">
+                ベータ版
+              </a>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card
+            :body-style="{'padding': '0'}">
+            <img
+              src="https://avatars2.githubusercontent.com/u/43202287?s=200&v=4"
+              class="image"
+              width="100%"
+              alt="aaa">
+            <div style="padding: 10px;">
+              <a
+                href="https://gallery.utakata-umigame.com"
+                target="_blank"
+                class="button--green">
+                リンク
+              </a>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col :span="8">
+          <el-card
+            :body-style="{'padding': '0'}">
+            <img
+              src="https://avatars2.githubusercontent.com/u/43202287?s=200&v=4"
+              class="image"
+              width="100%"
+              alt="aaa">
+            <div style="padding: 10px;">
+              <a
+                href="https://wolf.utakata-umigame.com"
+                target="_blank"
+                class="button--green">
+                リンク
+              </a>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </transition>
   </section>
 </template>
 <style>
